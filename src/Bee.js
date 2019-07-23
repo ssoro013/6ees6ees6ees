7cb4aaa9c0fc944d68b872423bb2e5026da96d1c
Soro Didiouf <ssoro013@gmail.com>
2019-07-23 16:17:12 -0700

    .

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  constructor(){
+    super()
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing'
+  }
 };
+
+//a "super" call in the constructor calls the constructor of the parent class
+//super.method(...) to call a parent method
+//must call the super constructor in derived class before accessing "this" or returning from derived constructor
